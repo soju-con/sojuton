a044ac63066876d7ee9429be33f9ae3879badeb5
caesiumy <dbs2636@gmail.com>
2023-08-08 04:52:16 +0900

    feat: Add EmailSection

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
+import EmailSection from "@/components/home/EmailSection";
 import FAQSection from "@/components/home/FAQSection";
 import HeroSection from "@/components/home/HeroSection";
 import IntroSection from "@/components/home/IntroSection";
 import LocationSection from "@/components/home/LocationSection";
 import ScheduleSection from "@/components/home/ScheduleSection";
 import SponsorSection from "@/components/home/SponsorSection";
-import React from "react";
 
 const HomePage = () => {
   return (
@@ -15,6 +15,7 @@ const HomePage = () => {
       <SponsorSection />
       <LocationSection />
       <FAQSection />
+      <EmailSection />
     </div>
   );
 };
---
+++ components/home/EmailSection.tsx
@@ -0,0 +1,35 @@
+import { Mail } from "lucide-react";
+import Link from "next/link";
+
+const SocialList = [
+  {
+    name: "이메일",
+    icon: <Mail />,
+    href: "mailto:soujucon.community@gmail.com",
+  },
+];
+
+const EmailSection = () => {
+  return (
+    <section className="p-4">
+      <div className="max-w-4xl p-8 my-8 mx-auto bg-base-200 rounded-xl">
+        <div className="flex flex-col gap-4 text-center">
+          <h2 className="font-climate-crisis text-5xl">Social</h2>
+          <p>더 궁금한 사항이 있으면 아래로 연락주세요!</p>
+          <div className="flex flex-wrap gap-2 justify-center">
+            {SocialList.map(({ name, icon, href }) => (
+              <Link key={name} href={href}>
+                <button className="btn btn-outline">
+                  {icon}
+                  {name}
+                </button>
+              </Link>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default EmailSection;
