eb4e2c488687d2a3cf90c158090197d6e83382a9
caesiumy <dbs2636@gmail.com>
2023-08-08 03:11:50 +0900

    feat: Update Header Menu button

--- components/home/HeroSection.tsx
+++ components/home/HeroSection.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { HandMetal } from "lucide-react";
 
 const HeroSection = () => {
   return (
@@ -7,13 +7,14 @@ const HeroSection = () => {
         <div className="flex flex-col gap-8 font-climate-crisis">
           <div className="space-y-8">
             <h3 className="text-2xl font-semibold">2023. 09. 02 BUSAN</h3>
-            <h1 className="text-7xl sm:text-[108px] md:text-[150px] font-black bg-gradient-to-b from-blue-50 to-blue-600 bg-clip-text text-transparent">
+            <h1 className="text-7xl sm:text-[108px] md:text-[150px] font-black bg-gradient-to-b from-blue-100 to-blue-600 bg-clip-text text-transparent">
               소주톤
             </h1>
             <p className="text-xl">얼레벌레 바캉스</p>
           </div>
           <div>
             <button className="btn btn-primary text-white">
+              <HandMetal />
               얼레벌레 하실분?
             </button>
           </div>
--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,5 +1,9 @@
+"use client";
+
+import { Menu } from "lucide-react";
 import Link from "next/link";
-import React from "react";
+import { usePathname, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const NavLinkItems = [
   {
@@ -17,27 +21,66 @@ const NavLinkItems = [
 ];
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const toggleMenu = () => setIsMenuOpen((prev) => !prev);
+
+  useEffect(() => {
+    setIsMenuOpen(false);
+  }, [pathname, searchParams]);
+
   return (
-    <header className="navbar bg-transparent backdrop-blur fixed top-0 z-40">
-      <div className="flex-1">
-        <Link
-          href="/"
-          className="btn btn-ghost normal-case text-2xl font-climate-crisis text-primary"
-        >
-          소주톤
-        </Link>
+    <header className="font-climate-crisis">
+      <div className="navbar bg-base-100 fixed top-0 z-40">
+        <div className="flex-1">
+          <Link
+            href="/"
+            className="btn btn-ghost normal-case text-2xl text-primary"
+          >
+            소주톤
+          </Link>
+        </div>
+        <nav className="flex-none hidden sm:block">
+          <ul className="menu menu-horizontal px-1 space-x-2">
+            {NavLinkItems.map(({ name, href }) => (
+              <li key={href}>
+                <Link href={href} className="font-semibold">
+                  {name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <button className="btn sm:hidden" onClick={toggleMenu}>
+          <Menu />
+        </button>
       </div>
-      <nav className="flex-none">
-        <ul className="menu menu-horizontal px-1 space-x-2">
-          {NavLinkItems.map(({ name, href }) => (
-            <li key={href}>
-              <Link href={href} className="font-semibold">
-                {name}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
+      <div className="sm:hidden">
+        <nav
+          className={`fixed left-0 right-0 z-30 bg-base-100 transition-all py-2 ${
+            isMenuOpen ? "top-16" : "-top-full"
+          }`}
+        >
+          <ul className="menu menu-horizontal px-1 flex flex-col gap-4 items-center">
+            {NavLinkItems.map(({ name, href }) => (
+              <li key={href}>
+                <Link href={href} className="font-semibold text-xl">
+                  {name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+
+        {isMenuOpen ? (
+          <div
+            className="fixed inset-0 bg-transparent z-20"
+            onClick={() => setIsMenuOpen(false)}
+          />
+        ) : null}
+      </div>
     </header>
   );
 };
