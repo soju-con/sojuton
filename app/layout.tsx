30f6d8965eb2226fcfbd70fd1de2891d6f6d86df
caesiumy <dbs2636@gmail.com>
2023-08-08 04:55:23 +0900

    feat: Add Footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { climateCrisisFont, pretendardFont } from "@/style/localFonts";
 import type { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/layout/Header";
+import Footer from "@/components/layout/Footer";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -21,6 +22,7 @@ export default function RootLayout({
       <body>
         <Header />
         <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
