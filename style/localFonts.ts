64f523b9479e297e2f132ce3f6c5b8c09e437b52
caesiumy <dbs2636@gmail.com>
2023-08-07 22:40:02 +0900

    feat: Add daisyUI and pretendard font

---
+++ style/localFonts.ts
@@ -0,0 +1,7 @@
+import localFont from "next/font/local";
+
+export const pretendardFont = localFont({
+  src: "../assets/fonts/PretendardVariable.woff2",
+  display: "swap",
+  variable: "--font-pretendard",
+});
