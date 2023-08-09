3713d6c0d20542625a10f4063d288551872c7110
caesiumy <dbs2636@gmail.com>
2023-08-09 23:38:02 +0900

    chore: Update party info

--- const/scheduleTable.ts
+++ const/scheduleTable.ts
@@ -1,33 +1,28 @@
 export const ScheduleTimeTable = [
   {
-    id: 1,
-    time: "08:00 - 09:30",
-    program: "운영진 집합 및 소주톤 운영 준비",
-  },
-  {
     id: 2,
     time: "09:30 - 10:00",
-    program: "등록 및 오프닝",
+    program: "해커톤 참가자 등록",
   },
   {
     id: 3,
     time: "10:00 - 12:00",
-    program: "해커톤 소개, 참가자 소개 및 아이스브레이킹, 팀 빌딩",
+    program: "해커톤과 참가자 소개 및 아이스브레이킹",
   },
   {
     id: 4,
     time: "12:00 - 13:00",
-    program: "점심",
+    program: "팀 빌딩 및 점심",
   },
   {
     id: 5,
     time: "13:00 - 19:00",
-    program: "해커톤 진행",
+    program: "해커톤 집중 모드",
   },
   {
     id: 6,
     time: "19:00 - 20:30",
-    program: "해커톤 발표",
+    program: "구현 내용 발표",
   },
   {
     id: 7,
@@ -37,11 +32,6 @@ export const ScheduleTimeTable = [
   {
     id: 8,
     time: "21:30 - 22:00",
-    program: "시상식 및 마무리",
-  },
-  {
-    id: 9,
-    time: "22:00 - 23:00",
-    program: "뒷정리",
+    program: "시상 및 마무리",
   },
 ];
